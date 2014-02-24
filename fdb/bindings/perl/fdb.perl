957a9639900d9cf42e3efdc35b3660266164beb6
Steve Duan <xdduan@gmail.com>
2014-02-24 15:48:38 -0500

    Added fdb_select_api_version() and fdb_get_error()

--- fdb/bindings/perl/fdb.perl
+++ fdb/bindings/perl/fdb.perl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
 require fdb;
 
-print fdb::fdb_get_max_api_version()
+print fdb::fdb_get_max_api_version(), "\n";
+print fdb::fdb_select_api_version(300, 300), "\n";
+print fdb::fdb_get_error(2203), "\n";
--- fdb/bindings/perl/fdb.pm
+++ fdb/bindings/perl/fdb.pm
@@ -2,8 +2,19 @@
 use fdb_c;
 package fdb;
 
-sub fdb_get_max_api_version {
+sub fdb_get_max_api_version
+{
 	return fdb_cc::fdb_get_max_api_version()
+}
+
+sub fdb_select_api_version
+{
+	return fdb_cc::fdb_select_api_version_impl($_[0], $_[1])
+}
+
+sub fdb_get_error
+{
+	return fdb_cc::fdb_get_error($_[0])
 }
 
 1;
